bb87f13b19b77b0ee169a9128ce78f24e18ed50b
mocnii <msestak@irb.hr>
2015-11-30 15:42:50 +0100

    test pod

--- README.md
+++ README.md
@@ -8,8 +8,6 @@ MySQLinstall - is installation script (modulino) that installs MySQL::Sandbox us
 
     MySQLinstall --mode=wget_mysql -url http://dev.mysql.com/get/Downloads/MySQL-5.5/mysql-5.5.43-linux2.6-x86_64.tar.gz
 
-    MySQLinstall --mode=wget_percona -url https://www.percona.com/downloads/Percona-Server-5.6/Percona-Server-5.6.24-72.2/binary/tarball/Percona-Server-5.6.24-rel72.2-Linux.x86_64.ssl101.tar.gz -url_tokudb https://www.percona.com/downloads/Percona-Server-5.6/Percona-Server-5.6.24-72.2/binary/tarball/Percona-Server-5.6.24-rel72.2-TokuDB.Linux.x86_64.ssl101.tar.gz
-
     MySQLinstall --mode=install_mysql -i ./download/mysql-5.6.26-linux-glibc2.5-x86_64.tar.gz
     MySQLinstall --mode=install_mysql --in=./download/Percona-Server-5.6.25-rel73.1-Linux.x86_64.ssl101.tar.gz
 
@@ -21,7 +19,7 @@ MySQLinstall - is installation script (modulino) that installs MySQL::Sandbox us
 
 # DESCRIPTION
 
-    MySQLinstall is installation script that installs Perl using plenv, MySQL::Sandbox using cpanm, MySQL in a sandbox, additional engines like TokuDB and Deep and updates configuration. 
+MySQLinstall is installation script that installs MySQL::Sandbox using cpanm, MySQL in a sandbox, additional engines like TokuDB and Deep and updates configuration for MySQL.
 
     --mode=mode                            Description
     --mode=install_sandbox         installs MySQL::Sandbox and prompts for modification of .bashrc
@@ -35,6 +33,44 @@ MySQLinstall - is installation script (modulino) that installs MySQL::Sandbox us
     MySQLinstall -h
     MySQLinstall -m
 
+## MODES
+
+- install\_sandbox
+
+        # options from command line
+        MySQLinstall --mode=install_sandbox --sandbox=$HOME/sandboxes/ --opt=$HOME/opt/mysql/
+
+        # options from config
+        MySQLinstall --mode=install_sandbox
+
+    Install MySQL::Sandbox, set environment variables (SANDBOX\_HOME and SANBOX\_BINARY) and create these directories if needed.
+
+- wget\_mysql
+
+        #option from command line (can also come from config)
+        MySQLinstall --mode=wget_mysql --url http://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-5.6.27-linux-glibc2.5-x86_64.tar.gz
+
+# CONFIGURATION
+
+All configuration in set in mysqlinstall.cnf that is found in ./lib directory. It follows [Config::Std](https://metacpan.org/pod/Config::Std) format and rules.
+Example:
+
+    [General]
+    sandbox  = /home/msestak/sandboxes
+    opt      = /home/msestak/opt/mysql
+    #url      = http://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-5.6.27-linux-glibc2.5-x86_64.tar.gz
+    url      = https://www.percona.com/downloads/Percona-Server-5.6/Percona-Server-5.6.27-75.0/binary/tarball/Percona-Server-5.6.27-rel75.0-Linux.x86_64.ssl101.tar.gz
+    out      = /msestak/gitdir/MySQLinstall
+    infile   = /msestak/gitdir/MySQLinstall/lib/MySQLinstall.pm
+    
+    [Database]
+    host     = localhost
+    database = test
+    user     = msandbox
+    password = msandbox
+    port     = 5625
+    socket   = /tmp/mysql_sandbox5625.sock
+
 # LICENSE
 
 Copyright (C) Martin Sebastijan Šestak.
@@ -47,9 +83,10 @@ it under the same terms as Perl itself.
 mocnii <msestak@irb.hr>
 
 # EXAMPLE
- MySQLinstall --mode=install\_mysql --in=./download/Percona-Server-5.6.25-rel73.1-Linux.x86\_64.ssl101.tar.gz
- MySQLinstall --mode=edit\_tokudb --opt=/home/msestak/opt/mysql/5.6.25/ --sand=/home/msestak/sandboxes/msb\_5\_6\_25/
 
+    MySQLinstall --mode=install_mysql --in=./download/Percona-Server-5.6.25-rel73.1-Linux.x86_64.ssl101.tar.gz
+    MySQLinstall --mode=edit_tokudb --opt=/home/msestak/opt/mysql/5.6.25/ --sand=/home/msestak/sandboxes/msb_5_6_25/
+    
     MySQLinstall --mode=install_mysql -i mysql-5.6.24-linux-glibc2.5-x86_64.tar.gz
     MySQLinstall --mode=edit_deep -i deep-mysql-5.6.24-community-plugin-3.2.0.19297-1.el6.x86_64.rpm --sand=/msestak/sandboxes/msb_5_6_24/ --opt=/msestak/opt/mysql/5.6.24/
 
--- lib/MySQLinstall.pm
+++ lib/MySQLinstall.pm
@@ -52,7 +52,7 @@ sub run {
     my $URL      = $param_href->{url};
     my $opt      = $param_href->{opt};
     my $sandbox  = $param_href->{sandbox};
-    my $INFILE   = $param_href->{infile};
+    my $infile   = $param_href->{infile};
     my $OUT      = $param_href->{out};         #not used
     my $HOST     = $param_href->{host};
     my $DATABASE = $param_href->{database};    #not used
@@ -490,6 +490,199 @@ sub wget_mysql {
 }
 
 
+### INTERFACE SUB ###
+# Usage      : install_mysql( $param_href );
+# Purpose    : installs MySQL binary using MySQL::Sandbox
+# Returns    : nothing
+# Parameters : ( $param_href ) -i from command line
+# Throws     : croaks if wrong number of parameters
+# Comments   : it modifies my.cnf for high performance too
+# See Also   :
+sub install_mysql {
+    my $log = Log::Log4perl::get_logger("main");
+    $log->logcroak ('install_mysql() needs a $param_href' ) unless @_ == 1;
+    my ( $param_href ) = @_;
+
+    my $infile = $param_href->{infile} or $log->logcroak( 'no $infile specified on command line!' );
+    my $mysql_binary = path($infile)->basename;   #need for install
+    $log->trace( "MySQL binary: $mysql_binary" );
+
+    (my $mysql_ver = $mysql_binary) =~ s/\A.+?-(5\.\d+\.\d+)-.+?\z/$1/;   #need for dir existence check
+    $log->trace( "MySQL version: $mysql_ver" );
+    (my $mysql_num = $mysql_ver) =~ s/\.//g;
+    $log->trace( "MySQL num: $mysql_num" );
+
+
+    my $opt_path = path($ENV{SANDBOX_BINARY}, $mysql_ver);   #need for dir existence check
+    $log->trace( "Specific SANDBOX_BINARY path: $opt_path" );
+
+    (my $sandbox_name = $mysql_ver) =~ s/\./_/g;
+    $log->trace( "Sandbox name: $sandbox_name" );
+
+    my $sandbox_path = path($ENV{SANDBOX_HOME}, 'msb_' . $sandbox_name);
+    $log->trace( "Specific SANDBOX_HOME path: $sandbox_path" );
+
+
+    #install MySQL with default options
+    if (-d $sandbox_path) {
+        $log->info( "sandbox $sandbox_path already exists" );
+        $log->info( "extracted $mysql_binary already exists in $opt_path" );
+        my $cmd_del = qq{sbtool -o delete -s $sandbox_path};
+        my ($stdout_del, $stderr_del, $exit_del) = capture_output( $cmd_del );
+        if ($exit_del == 0) {
+            $log->warn( "SANDBOX $sandbox_path deleted, deleting $opt_path too and installing new sandbox from fresh install" );
+			if (-d $opt_path) {
+				path($opt_path)->remove_tree and $log->warn( "OPT $opt_path deleted!" );
+			}
+            #fresh install
+			$log->info( "Installing $mysql_binary to $sandbox_path and extracting MySQL binary to $opt_path" );
+        	my $cmd_make = qq{make_sandbox --export_binaries $infile -- --no_confirm};    #infile needed for absolute path
+        	my ($stdout, $stderr, $exit) = capture_output( $cmd_make );
+        	if ($exit == 0) {
+        	        #install succeeded
+        	        $log->info( "Sandbox installed in $sandbox_path with MySQL in $opt_path" );
+        	}
+			else {
+				$log->error( "Action: MySQL failed to install to $sandbox_path with MySQL in $opt_path" );
+				$log->logexit( "Report: $stderr" );
+			}
+		}
+    }
+    else {
+        #fresh install
+        $log->info( "Installing $mysql_binary to $sandbox_path and extracting MySQL binary to $opt_path" );
+        my $cmd_make = qq{make_sandbox --export_binaries $infile -- --no_confirm};   #infile needed for absolute path
+        my ($stdout, $stderr, $exit) = capture_output( $cmd_make );
+        if ($exit == 0) {
+                #install succeeded
+                $log->info( "Sandbox installed in $sandbox_path with MySQL in $opt_path" );
+        }
+		else {
+			$log->error( "Action: MySQL failed to install to $sandbox_path with MySQL in $opt_path" );
+			$log->logexit( "Report: $stderr" );
+		}
+    }
+
+    #check my.cnf options
+    my $config_path = path($sandbox_path, 'my.sandbox.cnf');
+    read_config "$config_path" => my %config;
+    print Dumper(\%config);
+
+    my $basedir = $config{mysqld}{basedir};
+    if ($basedir eq $opt_path) {
+        $log->debug( "MySQL binaries are in right place: $basedir" );
+    }
+    else {
+        $log->warn( "MySQL binaries installed in wrong place: $basedir" );
+    }
+
+    my $datadir = $config{mysqld}{datadir};
+    my $path_to_datadir = path($sandbox_path, 'data');
+    if ($datadir eq $path_to_datadir) {
+        $log->debug( "MySQL data directory is in right place: $datadir" );
+    }
+    else {
+        $log->warn( "MySQL data directory installed in wrong place: $datadir" );
+    }
+
+    #change my.cnf options
+    open my $sandbox_cnf_fh, ">>", $config_path or $log->logdie( "Can't find cnf: $!" );
+
+my $cnf_options = <<"SQL";
+
+# MyISAM #
+key-buffer-size                = 32M
+myisam-recover-options         = FORCE,BACKUP
+
+# SAFETY #
+max-allowed-packet             = 16M
+max-connect-errors             = 1000000
+skip-name-resolve
+sql-mode                       = STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_AUTO_VALUE_ON_ZERO,NO_ENGINE_SUBSTITUTION,NO_ZERO_DATE,NO_ZERO_IN_DATE
+sysdate-is-now                 = 1
+innodb                         = FORCE
+innodb-strict-mode             = 1
+
+# TRANSACTION ISOLATION
+# default is: transaction-isolation = REPEATABLE-READ
+transaction-isolation          = READ-COMMITTED
+
+# BINARY LOGGING #
+#server-id                      = $mysql_num
+#log-bin                        = mysql-bin
+#expire-logs-days               = 14
+#sync-binlog                    = 1
+
+# CACHES AND LIMITS #
+tmp-table-size                 = 100M
+max-heap-table-size            = 100M
+query-cache-type               = 0
+query-cache-size               = 0
+max-connections                = 500
+thread-cache-size              = 50
+open-files-limit               = 65535
+table-definition-cache         = 1024
+table-open-cache               = 2048
+
+# INNODB #
+innodb-flush-method            = O_DIRECT
+innodb-log-files-in-group      = 2
+innodb-log-file-size           = 1G
+innodb-flush-log-at-trx-commit = 2
+innodb-file-per-table          = 1
+innodb-buffer-pool-size        = 1G
+
+# TIMEZONE #
+character_set_server           = latin1
+collation_server               = latin1_swedish_ci
+
+# LOGGING #
+slow-query-log                 = off
+#slow-query-log-file            = $sandbox_path/data/msandbox-slow.log
+#log-queries-not-using-indexes  = 1
+#long_query_time                = 0
+#log-error                     = $sandbox_path/data/msandbox.err
+
+# TokuDB #
+#tokudb_cache_size              = 1G
+#tokudb_data_dir                = $sandbox_path/data
+#tokudb_log_dir                 = $sandbox_path/data
+#tokudb_tmp_dir                 = $sandbox_path/data
+#tokudb_commit_sync             = 1
+#tokudb_directio                = 0
+#tokudb_load_save_space         = 1
+#default_storage_engine         = TokuDB
+#default_tmp_storage_engine     = TokuDB
+SQL
+
+    print {$sandbox_cnf_fh} $cnf_options, "\n";
+    $log->info("MySQL config $config_path modified for InnoDB" );
+    close $sandbox_cnf_fh;
+
+
+    #delete InnoDB logfiles (else it doesn't start)
+    foreach my $i (0..1) {
+        my $log_file = path($datadir, 'ib_logfile' . $i);
+        unlink $log_file and $log->trace( "InnoDB logfile $log_file deleted" ) 
+          or $log->logdie( "InnoDB file: $log_file not found: $!" );
+    }
+
+    #restart MySQl to check if config ok
+    my $cmd_restart = path($sandbox_path, 'restart');
+    my ($stdout_res, $stderr_res, $exit_res) = capture_output( $cmd_restart );
+        if ($exit_res == 0) {
+                #restart succeeded
+                $log->warn( "Sandbox $sandbox_path restarted with MySQL in $opt_path" );
+        }
+		else {
+			#restart failed
+			$log->logexit( "Sandbox $sandbox_path failed to restart AFTER updating InnoDB options" );
+		}
+
+    return;
+}
+
+
 
 1;
 __END__
@@ -506,8 +699,6 @@ MySQLinstall - is installation script (modulino) that installs MySQL::Sandbox us
 
  MySQLinstall --mode=wget_mysql -url http://dev.mysql.com/get/Downloads/MySQL-5.5/mysql-5.5.43-linux2.6-x86_64.tar.gz
 
- MySQLinstall --mode=wget_percona -url https://www.percona.com/downloads/Percona-Server-5.6/Percona-Server-5.6.24-72.2/binary/tarball/Percona-Server-5.6.24-rel72.2-Linux.x86_64.ssl101.tar.gz -url_tokudb https://www.percona.com/downloads/Percona-Server-5.6/Percona-Server-5.6.24-72.2/binary/tarball/Percona-Server-5.6.24-rel72.2-TokuDB.Linux.x86_64.ssl101.tar.gz
-
  MySQLinstall --mode=install_mysql -i ./download/mysql-5.6.26-linux-glibc2.5-x86_64.tar.gz
  MySQLinstall --mode=install_mysql --in=./download/Percona-Server-5.6.25-rel73.1-Linux.x86_64.ssl101.tar.gz
 
@@ -518,10 +709,9 @@ MySQLinstall - is installation script (modulino) that installs MySQL::Sandbox us
  MySQLinstall --mode=edit_deep_report -i ./download/deep-mysql-5.6.26-community-plugin-3.2.0.19896.el6.x86_64.tar.gz --sand=/home/msestak/sandboxes/msb_5_6_26 --opt=/home/msestak/opt/mysql/5.6.26
 
 
-
 =head1 DESCRIPTION
 
- MySQLinstall is installation script that installs Perl using plenv, MySQL::Sandbox using cpanm, MySQL in a sandbox, additional engines like TokuDB and Deep and updates configuration. 
+MySQLinstall is installation script that installs MySQL::Sandbox using cpanm, MySQL in a sandbox, additional engines like TokuDB and Deep and updates configuration for MySQL.
 
  --mode=mode				Description
  --mode=install_sandbox		installs MySQL::Sandbox and prompts for modification of .bashrc
@@ -535,6 +725,52 @@ MySQLinstall - is installation script (modulino) that installs MySQL::Sandbox us
  MySQLinstall -h
  MySQLinstall -m
 
+=head2 MODES
+
+=over 4
+
+=item install_sandbox
+
+ # options from command line
+ MySQLinstall --mode=install_sandbox --sandbox=$HOME/sandboxes/ --opt=$HOME/opt/mysql/
+
+ # options from config
+ MySQLinstall --mode=install_sandbox
+
+Install MySQL::Sandbox, set environment variables (SANDBOX_HOME and SANBOX_BINARY) and create these directories if needed.
+
+=item wget_mysql
+
+ #option from command line (can also come from config)
+ MySQLinstall --mode=wget_mysql --url http://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-5.6.27-linux-glibc2.5-x86_64.tar.gz
+
+=item install_mysql
+
+ #option from command line (can also come from config)
+ MySQLinstall --mode=install_mysql --infile mysql-5.6.27-linux-glibc2.5-x86_64.tar.gz
+
+=back
+
+=head1 CONFIGURATION
+
+All configuration in set in mysqlinstall.cnf that is found in ./lib directory. It follows L<Config::Std> format and rules.
+Example:
+
+ [General]
+ sandbox  = /home/msestak/sandboxes
+ opt      = /home/msestak/opt/mysql
+ #url      = http://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-5.6.27-linux-glibc2.5-x86_64.tar.gz
+ url      = https://www.percona.com/downloads/Percona-Server-5.6/Percona-Server-5.6.27-75.0/binary/tarball/Percona-Server-5.6.27-rel75.0-Linux.x86_64.ssl101.tar.gz
+ out      = /msestak/gitdir/MySQLinstall
+ infile   = /msestak/gitdir/MySQLinstall/lib/MySQLinstall.pm
+ 
+ [Database]
+ host     = localhost
+ database = test
+ user     = msandbox
+ password = msandbox
+ port     = 5625
+ socket   = /tmp/mysql_sandbox5625.sock
 
 =head1 LICENSE
 
@@ -548,6 +784,7 @@ it under the same terms as Perl itself.
 mocnii E<lt>msestak@irb.hrE<gt>
 
 =head1 EXAMPLE
+
  MySQLinstall --mode=install_mysql --in=./download/Percona-Server-5.6.25-rel73.1-Linux.x86_64.ssl101.tar.gz
  MySQLinstall --mode=edit_tokudb --opt=/home/msestak/opt/mysql/5.6.25/ --sand=/home/msestak/sandboxes/msb_5_6_25/
  
